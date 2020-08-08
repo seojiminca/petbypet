0836a98caf2849cf755f4a938cd7ef09db83e2f1
jimin <seojiminca@gmail.com>
2020-08-08 18:26:21 +0900

    Add: cats (update and delete)

--- server/cats/cat.controller.js
+++ server/cats/cat.controller.js
@@ -8,30 +8,30 @@ const checkAuth = passport.authenticate('jwt', {session: false});
 router.post('/', checkAuth, register);
 router.get('/', checkAuth, getAll);
 router.get('/:id', checkAuth, getById);
-//router.put('/:id', update);
-//router.delete('/:id', _delete);
+router.patch('/:id', checkAuth, update);
+router.delete('/:id', checkAuth, _delete);
 
 module.exports = router;
 
 //@route POST http://localhost:5000/cats/
 //@desc register cats
 //@access Private
-function register(req, res, next){
+function register(req, res, next) {
     const userId = req.user.id;
     catService.register(userId, req.body)
-      //.then(() => res.json({message:"success!"}))
-      .then(cat => res.json(cat))
-      .catch(err => next(err));
+        //.then(() => res.json({message:"success!"}))
+        .then(cat => res.json(cat))
+        .catch(err => next(err));
 }
 
 //@route GET http://localhost:5000/cats/
 //@desc get all cats
 //@access Private
 function getAll(req, res, next) {
-  const userId =req.user.id;
-  catService.getAll(userId)
-    .then(cats => res.json(cats))
-    .catch(err => next(err));
+    const userId = req.user.id;
+    catService.getAll(userId)
+        .then(cats => res.json(cats))
+        .catch(err => next(err));
 }
 
 
@@ -39,15 +39,26 @@ function getAll(req, res, next) {
 //@desc get a cat
 //@access Private
 function getById(req, res, next) {
-  catService.getById(res.params.id)
-    .then
+    catService.getById(req.params.id)
+        .then(cats => res.json(cats))
+        .catch(err => next(err));
 }
 
 //@route PATCH http://localhost:5000/cats/:id
 //@desc update cats
 //@access Private
+function update(req, res, next) {
+    catService.update(req.params.id, req.body)
+        .then(() => res.json({message: "Updated"}))
+        .catch(err => next(err));
+}
 
 
 //@route DELETE http://localhost:5000/cats/:id
 //@desc delete cats
 //@access Private
+function _delete(req, res, next) {
+    catService.delete(req.params.id)
+        .then(() => res.json({message: "Deleted"}))
+        .catch(err => next(err));
+}
--- server/cats/cat.service.js
+++ server/cats/cat.service.js
@@ -8,27 +8,27 @@ const validateLogin = require('../validation/login')
 const passport = require('passport');
 
 module.exports = {
-  register,
-  getAll,
-  getById,
-  update,
-  delete: _delete
+    register,
+    getAll,
+    getById,
+    update,
+    delete: _delete
 }
 
 //@route POST http://localhost:5000/cats/
 //@desc register cats
 //@access Private
 async function register(userId, catParam) {
-  const cat = new catModel(catParam);
-  cat.user = userId;
-  return await cat.save();
-} 
+    const cat = new catModel(catParam);
+    cat.user = userId;
+    return await cat.save();
+}
 
 //@route GET http://localhost:5000/cats/
 //@desc get all cats
 //@access Private
 async function getAll(userId) {
-  return await catModel.find();
+    return await catModel.find();
 }
 
 
@@ -36,23 +36,27 @@ async function getAll(userId) {
 //@desc get a cat
 //@access Private
 async function getById(id) {
-  return await catModel.findById(id);
+    return await catModel.findById(id);
 }
 
 //@route PATCH http://localhost:5000/cats/:id
 //@desc update cats
 //@access Private
 async function update(id, catParam) {
-  const cat = catModel.findById(id);
-  if(!cat) throw 'Cat not found.';
+    const cat = await catModel.findById(id);
+    //const cat = catModel.findOneAndUpdate(id);
+    console.log(cat);
 
-  Object.assign(cat, catParam);
-  await cat.save();
+    if (!cat) throw 'Cat not found.';
+
+    Object.assign(cat, catParam);
+
+    await cat.save();
 }
 
 //@route DELETE http://localhost:5000/cats/:id
 //@desc delete cats
 //@access Private
 async function _delete(id) {
-  await catModel.findByIdAndRemove(id);
+    await catModel.findByIdAndRemove(id);
 }
