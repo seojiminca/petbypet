b2dac00c612e429babe9c67b2b2151eb4728308f
yourankim <youran.dev@gmail.com>
2020-09-06 10:31:34 +0900

    Add: product.list

--- client/app/screen/product.list.js
+++ client/app/screen/product.list.js
@@ -1,6 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-const ProductList = () => (
-  <h1>Product List</h1>
-);
+const ProductList = () => {
+  const [data, setData] = useState({ products: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios("http://localhost:5000/products");
+      setData(result.data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <main className='p-4'>
+      <ul className='grid grid-cols-3 gap-6'>
+        {data.products.map((item) => (
+          <li key={item._id} className='h-50'>
+            <Link to={`/productdetail/${item._id}`}>
+              <div>
+                <img
+                  className='rounded'
+                  src='https://catlifetoday.com/wp-content/uploads/2019/05/dry-cat-food.jpg'
+                ></img>
+                <h3 className='text-center'>{item.name}</h3>
+                <h6 className='text-center text-gray-500'>{item.brand}</h6>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+};
 export default ProductList;
